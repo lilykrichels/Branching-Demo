0fb05f1087a0fc824f3c76c581ea03ed3b1b4cc6
lilykrichels <lily.krichels@gmail.com>
2017-05-30 11:25:18 -0400

    First Commmit

---
+++ FizzBuzzMay30/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzMay30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a FIZZ number.");
+            int FIZZ = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a BUZZ number.");
+            int BUZZ = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a number.");
+            int Num = int.Parse(Console.ReadLine());
+
+            for (int num = 1; num < 101; num++)
+            {
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("FIZZBUZZ!!");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("FIZZ!!");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("BUZZ!!");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+
+            }
+        }
+    }
+}
